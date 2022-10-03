78a9cc404731dfb9fc09f1809371100b7838c0fc
NastyaKurova <kyrova.nastia@gmail.com>
2022-10-03 11:36:18 +0300

    fix: dark mode

--- pages/index.tsx
+++ pages/index.tsx
@@ -19,8 +19,8 @@ const Home: NextPage = () => {
         <link rel="preconnect" href="https://fonts.googleapis.com"></link>
         <link rel="preconnect" href="https://fonts.gstatic.com"></link>
         <link
-          href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400;600&display=swap"
-          rel="stylesheet "></link>
+          href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400;600"
+          rel="preload stylesheet "></link>
       </Head>
 
       <main>
